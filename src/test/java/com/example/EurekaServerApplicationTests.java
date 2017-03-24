ddcff50e07acb487b55010ef9d24b23098f39b9d
xiaoshan <631910564@qq.com>
2017-03-24 13:31:51 +0800

    test

--- src/test/java/com/example/EurekaServerApplicationTests.java
+++ src/test/java/com/example/EurekaServerApplicationTests.java
@@ -3,10 +3,12 @@ package com.example;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableEurekaServer
 public class EurekaServerApplicationTests {
 
 	@Test
